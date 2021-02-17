182bb9dbdc0ee9575fc8f8604e95586523919044
baristure <baristure@hotmail.com>
2021-02-18 00:08:55 +0300

    Semantic UI added, routes created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container, Header } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
+import MenuBar from './components/MenuBar'
+import Home from './pages/Home'
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 function App() {
   return (
-    <div className="App">
-      <h1>Selam</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
